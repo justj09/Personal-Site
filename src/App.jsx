bff6a92d4b2697584558fe6b08b36431718ada79
justj09 <32967402+justj09@users.noreply.github.com>
2024-09-11 18:02:24 -0400

    add radio buttons for carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 import Sidebar from "./components/Sidebar.jsx";
 import Carousel from "./components/Carousel.jsx";
-import banner from './assets/Untitled.png';
 import ProjectCard from "./components/ProjectCard.jsx";
+import banner from './assets/banner.jpg';
 
 function App() {
   return (
-    <div id="page">
+    <div className="flex-horizontal">
       <Sidebar />
-      <div id="main-container">
+      <div className="flex-vertical flex-fill">
         <img id="banner" src={banner} />
         <div id="main-content">
           <div>
-            <Carousel>
-              <ProjectCard/>
-              <ProjectCard/>
-              <ProjectCard/>
-              <ProjectCard/>
-              <ProjectCard/>
-              <ProjectCard/>
-            </Carousel>
+            <h1>Title</h1>
+            <p>This is the content </p>
           </div>
+          <div className="divider"/>
+          <Carousel>
+            <ProjectCard/>
+            <ProjectCard/>
+            <ProjectCard/>
+            <ProjectCard/>
+            <ProjectCard/>
+            <ProjectCard/>
+          </Carousel>
+          <div className="divider"/>
         </div>
 
         <div id="spacer"/>
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -7,23 +7,28 @@ function Carousel(props) {
  
     function shiftLeft() {
         setIndex(previousState => {
-            return (previousState > 0) ? previousState - 1 : previousState;
+            return (previousState > 0) ? previousState - 1 : SIZE;
         });
     }
 
     function shiftRight() {
         setIndex(previousState => {
-            return (previousState < SIZE) ? previousState + 1 : previousState;
+            return (previousState < SIZE) ? previousState + 1 : 0;
         });
     }
 
     return (
         <div className="carousel">
-            <button onClick={shiftLeft}><IoIosArrowDropleftCircle /></button>
-            <div id="carousel-track">
-                {Children.map(props.children, child => <div className="track-item" style={{translate: index * (-100) + "%" }}>{child}</div>)}
+            <div className="flex-horizontal">
+                <button onClick={shiftLeft}><IoIosArrowDropleftCircle /></button>
+                <div id="carousel-track">
+                    {Children.map(props.children, child => <div className="track-item" style={{translate: index * (-100) + "%" }}>{child}</div>)}
+                </div>
+                <button onClick={shiftRight}><IoIosArrowDroprightCircle /></button>
             </div>
-            <button onClick={shiftRight}><IoIosArrowDroprightCircle /></button>
+            <div className="carousel-radio-container">
+                {[...Array(SIZE + 1).keys()].map((i) => <input type="radio" onClick={() => setIndex(i)} checked={i == index ? true : false}/>)}
+            </div>
         </div>
     )
 }
